0e306ed0e1f6bea39d291823e14e727f1e92026d
github-cloudlabsuser-1969 <github_cloudlabsuser_1969@cloudlabsaiuser.com>
2025-03-07 17:20:28 +0000

    skills.js

---
+++ skill.js
@@ -0,0 +1,9 @@
+// function convert Celcius to
+// Fahrenheit
+function convertCelciusToFahrenheit(celcius) {
+  return celcius * 9 / 5 + 32;
+}
+
+// Driver to code
+console.log(convertCelciusToFahrenheit(37));
+
